df72f70d1f44e1402f8bd328919074d6e756b166
Tallaneeraj <71711450+Tallaneeraj@users.noreply.github.com>
2025-05-08 11:55:44 -0700

    Create state_machine.py

---
+++ Data Ingestion using AWS/step-functions/state_machine.py
@@ -0,0 +1,19 @@
+{
+  "Comment": "Real-time Data Pipeline",
+  "StartAt": "InvokeLambda",
+  "States": {
+    "InvokeLambda": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:region:account-id:function:sensor-data",
+      "Next": "RunGlueJob"
+    },
+    "RunGlueJob": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun.sync",
+      "Parameters": {
+        "JobName": "sensor-data"
+      },
+      "End": true
+    }
+  }
+}
